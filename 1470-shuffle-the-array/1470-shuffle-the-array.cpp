5161776d9663dff8c4e1683e0ef1d6be749ed617
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2026-07-23 23:17:36 +0900

    Time: 4 ms (65.82%), Space: 13.2 MB (97.45%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,14 +3,19 @@ class Solution
 public:
     vector<int> shuffle(vector<int>& nums, int n)
     {
-        vector<int> ans;   // Creating a new array.
+        vector<int> ans(nums.size());   // Creating a new array.
 
-        for(int i = 0; i < 2 * n; ++i)   // Putting each corresponding elements per cycle.
+        // for(int i = 0; i < 2 * n; ++i)   // Putting each corresponding elements per cycle.
+        // {
+        //     if (i % 2 == 0) // For even-numbered indices.
+        //         ans.push_back(nums[i / 2]); // x1, x2, ...
+        //     else    // For odd-numbered indices.
+        //         ans.push_back(nums[n + i /2]);  // y1, y2, ...
+        // }
+
+        for(int i = 0; i < 2 * n; ++i)   // Or we can use the ternary operator here on behalf of vector functions.
         {
-            if (i % 2 == 0) // For even-numbered indices.
-                ans.push_back(nums[i / 2]); // x1, x2, ...
-            else    // For odd-numbered indices.
-                ans.push_back(nums[n + i /2]);  // y1, y2, ...
+            ans[i] = i % 2 == 0 ? nums[i / 2] : nums[n + i /2];
         }
 
         return ans;
