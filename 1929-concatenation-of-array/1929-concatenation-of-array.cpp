af7e156aec7cd8db375b63e06e2b08088e09f017
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2026-07-23 22:47:43 +0900

    Time: 0 ms (100%), Space: 17.2 MB (16.23%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,14 +1,27 @@
+// class Solution
+// {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums)
+//     {
+//         int n = nums.size();    // Storing the size of original array.
+//         vector<int> ans(2 * n);  // Creating a new array which is two times larger than the origin.
+        
+//         for(int i = 0; i < ans.size(); ++i) // Appending elements in original array into new one.
+//             ans[i] = nums[i % n];   // Using modulo calculation to pick appropriate character.
+        
+//         return ans;
+//     }
+// };
+
 class Solution
 {
 public:
     vector<int> getConcatenation(vector<int>& nums)
     {
-        int n = nums.size();    // Storing the size of original array.
-        vector<int> ans(2 * n);  // Creating a new array which is two times larger than the origin.
-        
-        for(int i = 0; i < ans.size(); ++i) // Appending elements in original array into new one.
-            ans[i] = nums[i % n];   // Using modulo calculation to pick appropriate character.
-        
-        return ans;
+        int n = nums.size();
+        for(int i = 0; i < n; ++i)
+            nums.push_back(nums[i]);
+
+        return nums;
     }
 };
