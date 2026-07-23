511f5f8d3f7c182eed649b8ca266fe893892eb0d
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2026-07-23 23:11:33 +0900

    Time: 6 ms (21.26%), Space: 13.5 MB (19.07%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -5,10 +5,12 @@ public:
     {
         vector<int> ans;   // Creating a new array.
 
-        for(int i = 0, j = n; i < n, j < 2 * n; ++i, ++j)   // Putting each corresponding elements per cycle.
+        for(int i = 0; i < 2 * n; ++i)   // Putting each corresponding elements per cycle.
         {
-            ans.push_back(nums[i]);  // x1, x2, ...
-            ans.push_back(nums[j]);  // y1, y2, ...
+            if (i % 2 == 0) // For even-numbered indices.
+                ans.push_back(nums[i / 2]); // x1, x2, ...
+            else    // For odd-numbered indices.
+                ans.push_back(nums[n + i /2]);  // y1, y2, ...
         }
 
         return ans;
